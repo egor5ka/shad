7b81f2353abb8f77ecb3594b25228d0ef4b7cc2d
Egor_ka <egor_5ka@yahoo.com>
2025-09-30 23:24:41 -0400

    edited war

--- war/war.h
+++ war/war.h
@@ -4,92 +4,78 @@
 #include <queue>
 #include <stdexcept>
 
-enum Winner { kFirst, kSecond, kNone };
+constexpr int MAX_TURNS = 1'000'000;
+constexpr int MIN_CARD = 0;
+constexpr int MAX_CARD = 9;
+
+enum class Winner { kFirst, kSecond, kNone };
 
 struct GameResult {
     Winner winner;
-    int turn;
+    int turn = 0;
 };
 
-inline std::tuple<std::queue<int>, std::queue<int>> ReadArray(const std::array<int, 5> &first_deck,
-                                                              const std::array<int, 5> &second_deck) {
-    std::queue<int> deck_que_first;
+inline std::queue<int> CreateQueue(const std::array<int, 5>& first_deck) {
+    std::queue<int> queue;
     for (auto& item : first_deck) {
-        deck_que_first.push(item);
+        queue.push(item);
     }
-    std::queue<int> deck_que_second;
-    for (auto& item : second_deck) {
-        deck_que_second.push(item);
-    }
-    return std::make_tuple(deck_que_first, deck_que_second);
+    return queue;
 }
 
-inline void CheckGameOver(const std::queue<int>& first_queue,
+inline std::optional<GameResult> CheckGameOver(const std::queue<int>& first_queue,
                           const std::queue<int>& second_queue,
-                          int counter,
-                          bool& GameOver,
-                          GameResult& result) {
+                          const int counter) {
 
     if (first_queue.empty() && !second_queue.empty()) {
-        GameOver = true;
-        result.winner = kSecond;
-        result.turn = counter;
-    } else if (second_queue.empty() && !first_queue.empty()) {
-        GameOver = true;
-        result.winner = kFirst;
-        result.turn = counter;
-    } else if (first_queue.empty() && second_queue.empty()) {
-        GameOver = true;
-        result.winner = kNone;
-        result.turn = counter;
-        return;
+        return GameResult{Winner::kSecond, counter};
     }
-    if (counter >= 1000000) {
-        GameOver = true;
-        result.winner = kNone;
-        result.turn = counter;
+    if (second_queue.empty() && !first_queue.empty()) {
+        return GameResult{Winner::kFirst, counter};
+    }
+    if (first_queue.empty() && second_queue.empty()) {
+        return GameResult{Winner::kNone, counter};
     }
 
+    return std::nullopt;
+}
+
+inline void PushCards(std::queue<int>* queue, int move_first, int move_second) {
+    queue->push(move_first);
+    queue->push(move_second);
 }
 
 GameResult SimulateWarGame(const std::array<int, 5>& first_deck,
                            const std::array<int, 5>& second_deck) {
 
-    auto [first_queue, second_queue] = ReadArray(first_deck, second_deck);
+    std::queue<int> first_queue = CreateQueue(first_deck);
+    std::queue<int> second_queue = CreateQueue(second_deck);
 
-    size_t counter = 0;
-    bool GameOver = false;
-    GameResult result;
+    int counter = 0;
 
-    while (!GameOver) {
-        CheckGameOver(first_queue, second_queue, counter, GameOver, result);
-        if (GameOver) {
-            break;
-        }
+    while (counter < MAX_TURNS) {
 
         int move_first = first_queue.front();
         first_queue.pop();
+
         int move_second = second_queue.front();
         second_queue.pop();
 
         ++counter;
 
-        if ((move_first == 0 && move_second == 9)) {
-            first_queue.push(move_first);
-            first_queue.push(move_second);
+        const bool first_wins = (move_first == MIN_CARD && move_second == MAX_CARD) ||
+    (move_first > move_second && !(move_first == MAX_CARD && move_second == MIN_CARD));
+
+        if (first_wins) {
+            PushCards(&first_queue, move_first, move_second);
+        } else {
+            PushCards(&second_queue, move_first, move_second);
         }
-        else if ((move_second == 0 && move_first == 9)) {
-            second_queue.push(move_first);
-            second_queue.push(move_second);
-        }
-        else if (move_first > move_second) {
-            first_queue.push(move_first);
-            first_queue.push(move_second);
-        }
-        else if (move_second > move_first) {
-            second_queue.push(move_first);
-            second_queue.push(move_second);
+
+
+        if (auto check =  CheckGameOver(first_queue, second_queue, counter); check.has_value()) {
+            return GameResult{check->winner, check->turn};
         }
     }
-    return result;
+    return {Winner::kNone, counter};
 }
