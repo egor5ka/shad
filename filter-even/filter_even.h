17f620d22d862fba9ce72e7440721840d511ec04
Egor_ka <egor_5ka@yahoo.com>
2025-10-01 17:29:39 -0400

    modified filter_even

--- filter-even/filter_even.h
+++ filter-even/filter_even.h
@@ -1,13 +1,28 @@
 #pragma once
 
 #include <vector>
-#include <algorithm>
+#include <iostream>
 #include <stdexcept>
 
 inline void FilterEven(std::vector<int> *data) {
-    auto is_even = [](int n) {
-        return n % 2 == 0;
-    };
+    // data -> erase(std::remove_if(data->begin(), data->end(),
+    //     [](int i){ return i % 2 != 0; }), data->end());
 
-    data->erase(std::remove_if(data->begin(), data->end(), is_even), data->end());
+    // for (size_t i = 0; i < data->size();) {
+    //     if ((*data)[i] % 2 != 0) {
+    //         data -> erase(data->begin() + i);
+    //     } else {
+    //         ++i;
+    //     }
+    // }
+
+    size_t write = 0;
+
+    for (size_t read = 0; read < data->size(); ++read) {
+        if ((*data)[read] % 2 == 0) {
+            (*data)[write] = (*data)[read];
+            ++write;
+        }
+    }
+    data -> resize(write);
 }
